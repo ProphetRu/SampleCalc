e82cccf8933ac9c48b427f3ecf03c0d6b5ce6c7a
ProphetRu <155024765+ProphetRu@users.noreply.github.com>
2024-01-02 15:17:05 +0300

    Fix template

--- src/SampleCalc.hpp
+++ src/SampleCalc.hpp
@@ -6,25 +6,21 @@ template<typename T>
 class SampleCalc final
 {
 public:
-	template <typename T>
 	static T Plus(const T& a, const T& b) noexcept
 	{
 		return a + b;
 	}
 
-	template <typename T>
 	static T Minus(const T& a, const T& b) noexcept
 	{
 		return a - b;
 	}
 
-	template <typename T>
 	static T Multiply(const T& a, const T& b) noexcept
 	{
 		return a * b;
 	}
 
-	template <typename T>
 	static T Divide(const T& a, const T& b)
 	{
 		if (b == 0)
